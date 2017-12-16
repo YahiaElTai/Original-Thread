966a7e8f060aa8c004773e55d7dbfdc2747e819f
Yahia El Tai <yahiaeltai114@gmail.com>
2017-12-16 23:49:03 +0100

    add text functionality almost done

--- app/containers/ProductCreator.jsx
+++ app/containers/ProductCreator.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Textfit } from 'react-textfit';
 import { Link } from 'react-router';
 let { connect } = require('react-redux');
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
 import Rnd from 'react-rnd';
 let actions = require('productActions');
+import { ChromePicker } from 'react-color';
+import FontPicker from 'react-font-picker';
 
 class ProductCreator extends React.Component {
   constructor() {
     super();
-
     this.formData = new FormData();
+    this.validateText = this.validateText.bind(this);
     this.state = {
-      activeImage: "https://cdn.shopify.com/s/files/1/2485/3042/products/Bella_Canvas_8413_Triblend_Short_Sleeve_T-shirt_-_Clay.png?v=1509004973",
+      activeImage: 'https://cdn.shopify.com/s/files/1/2485/3042/products/Anvil_880_Ringspun_Fashion_Fit_T-shirt_-_White.png?v=1509003615',
       file: null,
       fileCoordinates: {
         x: 0,
@@ -22,7 +25,29 @@ class ProductCreator extends React.Component {
         width: 150,
         height: 150
       },
-      dropzoneActive: false
+
+      dropzoneActive: false,
+      text: 'Yo Mama So...',
+      textCoordinates: {
+        x: 50,
+        y: 219
+      },
+      textDimensions: {
+        width: 367,
+        height: 94
+      },
+      fontSize: '51.7',
+      textColor: '#000000',
+      boldStyle: 'normal',
+      italicStyle: 'normal',
+      showFontPicker: false,
+      fontFamily: 'Arial',
+      aspectRatio: 0.256,
+      showTextField: false,
+      errorMessage: false,
+      transformMode: true,
+      editMode: false,
+      showColorPicker: false
     };
   }
 
@@ -39,7 +64,15 @@ class ProductCreator extends React.Component {
   }
 
   handleCreateImage() {
-
+    let text = this.state.text;
+    let aspectRatio = this.state.aspectRatio;
+    let textCoordinates = this.state.textCoordinates;
+    let textDimensions = this.state.textDimensions;
+    let fontSize = this.state.fontSize;
+    let textColor = this.state.textColor;
+    let boldStyle = this.state.boldStyle;
+    let italicStyle = this.state.italicStyle;
+    let fontFamily = this.state.fontFamily;
     let projectTitle = this.refs.projectTitle.value;
     let projectDescription = this.refs.projectDescription.value;
     let backgroundImage = this.state.activeImage;
@@ -48,6 +81,15 @@ class ProductCreator extends React.Component {
     let fileCoordinates = this.state.fileCoordinates;
 
     let data = {
+      text,
+      aspectRatio,
+      textCoordinates,
+      textDimensions,
+      fontSize,
+      textColor,
+      boldStyle,
+      italicStyle,
+      fontFamily,
       projectTitle,
       projectDescription,
       backgroundImage,
@@ -55,7 +97,6 @@ class ProductCreator extends React.Component {
       fileDimensions,
       fileCoordinates
     };
-
     console.log('creating image...', data);
 
     axios
@@ -67,18 +108,18 @@ class ProductCreator extends React.Component {
       .then(response => {
         console.log('success creating image!: ', response);
         const newProduct = response.data;
-        console.log("newProduct: ", newProduct);
+        console.log('newProduct: ', newProduct);
 
         return axios
-          .put('/shopify/update_live_feed', newProduct , {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(response => {
-              console.log('producted added succes!');
-        })
-        .catch( error => console.log('Error adding product: ', error));
+          .put('/shopify/update_live_feed', newProduct, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => {
+            console.log('producted added succes!');
+          })
+          .catch(error => console.log('Error adding product: ', error));
       })
       .catch(error => console.log('Error creating image file: ', error));
   }
@@ -104,34 +145,76 @@ class ProductCreator extends React.Component {
       .catch(error => console.log('Error uploading file: ', error));
   }
 
-  handleOnResize(e, direction, ref, delta, position){
-     this.setState({
+  handleOnResize(e, direction, ref, delta, position) {
+    this.setState({
       fileDimensions: {
         width: ref.offsetWidth,
-        height: ref.offsetHeight,
+        height: ref.offsetHeight
       },
-       fileCoordinates: {
+      fileCoordinates: {
         ...position
-       }
+      }
+    });
+  }
 
-     });
-      console.log("this.state from onResize:", this.state);
+  handleTextOnResize(e, direction, ref, delta, position) {
+    let { aspectRatio, textDimensions } = this.state;
+    this.setState({
+      editMode: false,
+      fontSize: 0.55 * ref.offsetHeight,
+      textDimensions: {
+        width: ref.offsetWidth,
+        height: ref.offsetHeight
+      },
+      textCoordinates: {
+        ...position
+      }
+    });
 
+    this.validateText(position.x, position.y, ref.offsetWidth);
+  }
+
+  getSize(e) {
+    let { fontSize, textDimensions, textCoordinates } = this.state;
+    let textValue = e.target.value;
+    let size = textValue.length;
+    let height = textDimensions.height;
+
+    if (size === 0) {
+      this.setState({
+        fontSize: '16.5',
+        textDimensions: { height: 30, width: 117 }
+      });
+    }
+
+    this.setState({
+      text: textValue,
+      textDimensions: { width: size * (10 * fontSize / 16.5), height: height },
+      aspectRatio: height / (size * (10 * fontSize) / 16.5)
+    });
+
+    this.validateText(textCoordinates.x, textCoordinates.y, textDimensions.width);
   }
 
   handledragged(e, d) {
     let x = d.x;
     let y = d.y;
-    console.log('x: ', x);
-    console.log('y: ', y);
 
     this.setState({ fileCoordinates: { x, y } });
   }
 
+  handleTextdragged(e, d) {
+    let { textDimensions } = this.state;
+    let x = d.x;
+    let y = d.y;
+
+    this.validateText(x, y, textDimensions.width);
+    this.setState({ textCoordinates: { x, y }, editMode: false });
+  }
+
   handleActiveColor(index) {
     let { activeProduct } = this.props;
-      console.log("index:" , index);
-      this.setState({activeImage: activeProduct.options.images[index].src});
+    this.setState({ activeImage: activeProduct.options.images[index].src });
   }
 
   colorList() {
@@ -140,7 +223,7 @@ class ProductCreator extends React.Component {
     if (activeProduct) {
       let colorArray = activeProduct.options.options;
       if (colorArray.length > 1) {
-        return activeProduct.options.options[1].values.map( (color, index) => {
+        return activeProduct.options.options[1].values.map((color, index) => {
           return <div onClick={this.handleActiveColor.bind(this, index)} className="pc-product-color-item" style={{ backgroundColor: `${color}` }} />;
         });
       }
@@ -149,64 +232,180 @@ class ProductCreator extends React.Component {
 
   componentDidMount() {
     let { activeProduct } = this.props;
-    this.setState({activeImage: activeProduct.options.images[0].src});
+    this.setState({ activeImage: activeProduct.options.images[0].src });
   }
 
+  validateText(x, y, width) {
+    if (x < 140) {
+      this.setState({ errorMessage: true });
+    } else if (x > 140 && width > 205) {
+      this.setState({ errorMessage: true });
+    } else if (x > 280) {
+      this.setState({ errorMessage: true });
+    } else if (x > 200 && width > 110) {
+      this.setState({ errorMessage: true });
+    } else if (y < 180 || y > 339) {
+      this.setState({ errorMessage: true });
+    } else {
+      this.setState({ errorMessage: false });
+    }
+  }
+
+  handleMouseEnter() {
+    this.setState({ transformMode: true });
+  }
+
+  exitTransformMode() {
+    this.setState({ transformMode: false });
+  }
+
+  handleChangeComplete(color, event) {
+    this.setState({ textColor: color.hex });
+  }
+
+  handleDoubleClick() {
+    this.setState({ editMode: true, transformMode: false });
+  }
+
+  exitEditMode() {
+    this.setState({ editMode: false, showColorPicker: false, showFontPicker: false });
+  }
+
+  handleColorPicker() {
+    let { showColorPicker } = this.state;
+    this.setState({ showColorPicker: !showColorPicker });
+  }
+
+  handleBoldStyle() {
+    let { boldStyle } = this.state;
+    if (boldStyle === 'normal') {
+      this.setState({ boldStyle: 'bold' });
+    } else {
+      this.setState({ boldStyle: 'normal' });
+    }
+  }
+
+  handleItalicStyle() {
+    let { italicStyle } = this.state;
+    if (italicStyle === 'normal') {
+      this.setState({ italicStyle: 'italic' });
+    } else {
+      this.setState({ italicStyle: 'normal' });
+    }
+  }
+
+  handleShowFontPicker() {
+    let { showFontPicker } = this.state;
+    this.setState({ showFontPicker: !showFontPicker });
+  }
+
+  handleFontChange(selectedFont) {
+    let { fontFamily } = this.state;
+    this.setState({ fontFamily: selectedFont });
+  }
 
   render() {
+    let fontsArray = [
+      'Arial',
+      'Arial Black',
+      'Comic Sans MS',
+      'cursive',
+      'Impact',
+      'Charcoal',
+      'Lucida Sans Unicode',
+      'Times New Roman',
+      'Lucida Grande',
+      'Tahoma',
+      'Geneva',
+      'Trebuchet MS',
+      'Helvetica',
+      'Verdana',
+      'Geneva',
+      'Courier New',
+      'Courier',
+      'Lucida Console',
+      'Monaco'
+    ];
 
-    let { activeProduct} = this.props;
+    let { activeProduct } = this.props;
+    console.log('activeProduct:', activeProduct);
+    console.log(this.state.text);
     let rndStyles = {};
-    let { file,  dropzoneActive , activeImage} = this.state;
+    let {
+      aspectRatio,
+      fontSize,
+      showTextField,
+      textDimensions,
+      textCoordinates,
+      file,
+      fileCoordinates,
+      fontFamily,
+      showColorPicker,
+      showFontPicker,
+      boldStyle,
+      italicStyle,
+      editMode,
+      fileDimensions,
+      dropzoneActive,
+      textColor,
+      activeImage,
+      errorMessage,
+      transformMode
+    } = this.state;
     let dropzoneRef;
-    if (file) {
-      rndStyles = {
-        backgroundImage: `url('uploads/${file}')`,
-        backgroundPosition: 'center center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain'
-
-      };
-    };
 
     return (
       <div className="pc">
         <div className="pc-container">
           <div className="pc-left">
-
-          <div className="btn-group">
-              <div className="btn-item" onClick={() => { dropzoneRef.open(); }}>
-                <i className="fa fa-camera" aria-hidden="true"></i>
+            <div className="btn-group">
+              <div
+                className="btn-item"
+                onClick={() => {
+                  dropzoneRef.open();
+                }}
+              >
+                <i className="fa fa-camera" aria-hidden="true" />
                 <p>ARTWORK</p>
               </div>
               <div className="btn-item">
-                <i className="fa fa-bomb" aria-hidden="true"></i>
+                <i className="fa fa-bomb" aria-hidden="true" />
                 <p>DESIGNLIBRARY</p>
               </div>
-              <div className="btn-item">
-                <i className="fa fa-font" aria-hidden="true"></i>
+              <div
+                className="btn-item"
+                onClick={() => {
+                  this.setState({ showTextField: !showTextField });
+                }}
+              >
+                <i className="fa fa-font" aria-hidden="true" />
                 <p>TEXT</p>
               </div>
-          </div>
+            </div>
 
-          <div className="product-details">
-            <h6>Project Title</h6>
-            <input type="text" className="project-title"  ref="projectTitle" defaultValue="Yo Momma's Bounds"/>
+            <div className="product-details">
+              <h6>Project Title</h6>
+              <input type="text" className="project-title" ref="projectTitle" defaultValue="Yo Momma's Bounds" />
 
-            <h6>Description</h6>
-            <textarea type="text" className="project-description"  ref="projectDescription" defaultValue="with a seamless product creation app, Original Thread will definitely take all the heavy lifting and leave you the creative process..."></textarea>
-          </div>
-
-
+              <h6>Description</h6>
+              <textarea
+                type="text"
+                className="project-description"
+                ref="projectDescription"
+                defaultValue="with a seamless product creation app, Original Thread will definitely take all the heavy lifting and leave you the creative process..."
+              />
+            </div>
           </div>
           <div className="pc-middle">
-            <div className="preview"
+            <div
+              className="preview"
               style={{
-                backgroundImage: `url('${activeImage ? activeImage : ""} ')`
+                backgroundImage: `url('${activeImage ? activeImage : ''} ')`
               }}
             >
+              <p className={errorMessage ? 'error-text' : 'hide'}>Your graphic is beyond recommended size</p>
               <Dropzone
-                disableClick={file ? true : false}
+                disableClick={file || showTextField ? true : false}
                 onDrop={this.onDrop.bind(this)}
                 onDragEnter={this.onDragEnter.bind(this)}
                 onDragLeave={this.onDragLeave.bind(this)}
@@ -216,23 +415,90 @@ class ProductCreator extends React.Component {
                   dropzoneRef = node;
                 }}
               >
-               <Rnd
-                 style={ file ? rndStyles : ''}
-                 default={{
-                   x: this.state.fileCoordinates.x,
-                   y: this.state.fileCoordinates.y,
-                   width: this.state.fileDimensions.width,
-                   height: this.state.fileDimensions.height
-                 }}
-                 size={{ width: this.state.fileDimensions.width,  height: this.state.fileDimensions.height }}
-                 position={{ x: this.state.fileCoordinates.x, y: this.state.fileCoordinates.y }}
-                 lockAspectRatio = "true"
-                 bounds="parent"
-                 onResize={this.handleOnResize.bind(this)}
-                 onDragStop={this.handledragged.bind(this)}
-               >
-               </Rnd>
+                <Rnd
+                  style={file ? { backgroundImage: `url('uploads/${file}')` } : ''}
+                  default={{
+                    x: fileCoordinates.x,
+                    y: fileCoordinates.y,
+                    width: fileDimensions.width,
+                    height: fileDimensions.height
+                  }}
+                  size={{ width: fileDimensions.width, height: fileDimensions.height }}
+                  position={{ x: fileCoordinates.x, y: fileCoordinates.y }}
+                  lockAspectRatio="true"
+                  className="background-upload"
+                  bounds="parent"
+                  onResize={this.handleOnResize.bind(this)}
+                  onDragStop={this.handledragged.bind(this)}
+                />
               </Dropzone>
+              <Rnd
+                default={{
+                  x: textCoordinates.x,
+                  y: textCoordinates.y,
+                  width: textDimensions.width,
+                  height: textDimensions.height
+                }}
+                size={{ width: textDimensions.width, height: textDimensions.height }}
+                position={{ x: textCoordinates.x, y: textCoordinates.y }}
+                lockAspectRatio={aspectRatio}
+                bounds="parent"
+                className={showTextField ? 'show' : 'hide'}
+                onResize={this.handleTextOnResize.bind(this)}
+                onDrag={this.handleTextdragged.bind(this)}
+              >
+                <div className="input-wrapper">
+                  <input
+                    onMouseEnter={this.handleMouseEnter.bind(this)}
+                    onDoubleClick={this.handleDoubleClick.bind(this)}
+                    className={transformMode ? 'text-input bordered' : 'text-input'}
+                    type="text"
+                    onChange={this.getSize.bind(this)}
+                    value={this.state.text}
+                    style={{ fontSize: fontSize + 'px', color: textColor, fontWeight: boldStyle, fontStyle: italicStyle, fontFamily: fontFamily }}
+                  />
+                  <i className={transformMode ? 'fa fa-expand' : ''} aria-hidden="true" />
+                  <i onClick={this.exitTransformMode.bind(this)} className={transformMode ? 'fa fa-times' : ''} aria-hidden="true" />
+                </div>
+              </Rnd>
+              <Rnd
+                default={{
+                  x: textCoordinates.x,
+                  y: textCoordinates.y + 82,
+                  width: 367,
+                  height: 40
+                }}
+                position={{ x: textCoordinates.x, y: textCoordinates.y + 82 }}
+                lockAspectRatio="true"
+                bounds="parent"
+              >
+                <div className={editMode ? 'text-transform-group' : 'hide'}>
+                  <div onClick={this.handleShowFontPicker.bind(this)} className="text-item-1">
+                    Font Family
+                  </div>
+                  <div onClick={this.handleBoldStyle.bind(this)} className="text-item-2">
+                    B
+                  </div>
+                  <div onClick={this.handleItalicStyle.bind(this)} className="text-item-3">
+                    I
+                  </div>
+                  <div onClick={this.handleColorPicker.bind(this)} className="text-item-4">
+                    Color
+                  </div>
+                  <div onClick={this.exitEditMode.bind(this)} className="text-item-5">
+                    OK
+                  </div>
+                </div>
+
+                <ChromePicker
+                  className={showColorPicker ? 'color-picker' : 'hide'}
+                  color={this.state.textColor}
+                  onChangeComplete={this.handleChangeComplete.bind(this)}
+                />
+                <div className={showFontPicker ? 'show' : 'hide'}>
+                  <FontPicker fonts={fontsArray} previews={true} activeColor="#64B5F6" value={fontFamily} onChange={this.handleFontChange.bind(this)} />
+                </div>
+              </Rnd>
             </div>
           </div>
           <div className="pc-right">
@@ -240,16 +506,18 @@ class ProductCreator extends React.Component {
               <Link to="/product_array">
                 <img src="/images/PRODUCT ARRAY TRIGGER.png" alt="Product Array Trigger" />
               </Link>
-              <h4>{activeProduct ? activeProduct.options.title : ""}</h4>
-              <p>60% Cotton  30% Polyster</p>
+              <h4>{activeProduct ? activeProduct.options.title : ''}</h4>
+              <p>60% Cotton 30% Polyster</p>
             </div>
             <div className="pc-product-color-group">{this.colorList()}</div>
           </div>
         </div>
         <div className="decisions">
-          <i className="fa fa-refresh" aria-hidden="true"></i>
+          <i className="fa fa-refresh" aria-hidden="true" />
           <button className="save-work"> Save Work</button>
-          <button type="button" className="publish" onClick={this.handleCreateImage.bind(this)}>Publish</button>
+          <button type="button" className="publish" onClick={this.handleCreateImage.bind(this)}>
+            Publish
+          </button>
         </div>
       </div>
     );
@@ -262,5 +530,3 @@ export default connect(state => {
     collections: state.collections.all
   };
 })(ProductCreator);
-
-
--- routes/shopify.js
+++ routes/shopify.js
@@ -2,10 +2,8 @@ let express = require('express');
 let router = express.Router();
 let Shopify = require('shopify-api-node');
 const download = require('image-downloader');
-var request = require('request');
 let fs = require('fs');
 let gm = require('gm').subClass({ imageMagick: true });
-
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
@@ -38,6 +36,12 @@ router.route('/').get(function(req, res) {
 
 // Create new product image
 router.route('/new_product_image').post(function(req, res) {
+  const font = 2 * req.body.fontSize;
+  const text = req.body.text;
+  const textColor = req.body.textColor;
+  const textCoordinates = req.body.textCoordinates;
+  const textRealXCoord = 100 + 1.75 * textCoordinates.x;
+  const textRealYCoord = 100 + 1.75 * textCoordinates.y;
   const projectTitle = req.body.projectTitle;
   const projectDescription = req.body.projectDescription;
 
@@ -49,79 +53,82 @@ router.route('/new_product_image').post(function(req, res) {
 
   const uploadedImageWidth = 2 * fileDimensions.width;
   const uploadedImageHeight = 2 * fileDimensions.height;
-  const uploadedImageOffsetX = 291 +  2 * fileCoordinates.x;
-  const uploadedImageOffsetY = 291 +  2 * fileCoordinates.y;
+  const uploadedImageOffsetX = 291 + 2 * fileCoordinates.x;
+  const uploadedImageOffsetY = 291 + 2 * fileCoordinates.y;
 
-   const options = {
-     url: backgroundImage,
-     dest: 'public/images/backgroundImage.jpg'
-   }
+  const options = {
+    url: backgroundImage,
+    dest: 'public/images/backgroundImage.jpg'
+  };
 
-   download.image(options)
-     .then(({ filename, image }) => {
-       console.log('File saved to', filename);
+  download
+    .image(options)
+    .then(({ filename, imageFile }) => {
+      console.log('File saved to', filename);
 
-       gm("public/images/backgroundImage.jpg")
-         .composite(uploadedImage)
-         .geometry(`${uploadedImageWidth}x${uploadedImageHeight}+${uploadedImageOffsetX}+${uploadedImageOffsetY}`)
-         .write("public/images/new_image.png" , function(err) {
-           if (err) {
-             console.log('image conversion error!');
-             console.log('er: ', err);
-           } else {
-             console.log('image converted success :');
+      let image = gm('public/images/backgroundImage.jpg');
 
+      if (uploadedImage !== 'public/uploads/null') {
+        image = image
+          .out(uploadedImage)
+          .geometry(`${uploadedImageWidth}x${uploadedImageHeight}+${uploadedImageOffsetX}+${uploadedImageOffsetY}`)
+          .out('-composite');
+      }
+      if (text !== 'Yo Mama So...') {
+        image = image
+          .fill(textColor)
+          .font('../app/fonts/roboto-regular.eot', font)
+          .drawText(textRealXCoord, textRealYCoord, text);
+      }
 
-             shopify.product.create({
-                                 "title": projectTitle,
-                                 "body_html": projectDescription,
-                                 "vendor": "Original Thread",
-                                 "template_suffix": "product.liquid",
-                                 "images": [
-                                       {
-                                         "attachment": base64_encode("public/images/new_image.png")
-                                       }
-                                       ]
-                               })
-                               .then(product => {
-                                console.log('product created success: ', product)
-                                res.send(product);
-                                })
+      image.write('public/images/new_image.png', function(err) {
+        if (err) {
+          console.log('image conversion error!');
+          console.log('er: ', err);
+        } else {
+          console.log('image converted success :');
+          shopify.product
+            .create({
+              title: projectTitle,
+              body_html: projectDescription,
+              vendor: 'Original Thread',
+              template_suffix: 'product.liquid',
+              images: [
+                {
+                  attachment: base64_encode('public/images/new_image.png')
+                }
+              ]
+            })
+            .then(product => {
+              console.log('product created success: ', product);
+              res.send(product);
+            })
+            .catch(err => console.error('shopify create err:', err));
+        }
+      });
+    })
 
-                               .catch(err => console.error('shopify create err:', err));
-           }
-         });
-
-     }).catch((err) => {
-       console.log("download err: ", err);
-     })
-
-
-
-
-
+    .catch(err => {
+      console.log('download err: ', err);
+    });
 });
 
-  router.route('/update_live_feed').put(function(req, res) {
+router.route('/update_live_feed').put(function(req, res) {
+  const productID = req.body.id;
+  shopify.customCollection
+    .update(10178953249, {
+      collects: [
+        {
+          product_id: productID,
+          position: 1
+        }
+      ]
+    })
+    .then(addedProduct => {
+      console.log('product added success: ', addedProduct);
+    })
 
-    const productID = req.body.id;
-    console.log("newProduct: ", productID);
-
-
-    shopify.customCollection.update(10178953249, {
-                                                  "collects": [
-                                                    {
-                                                      "product_id": productID,
-                                                      "position": 1
-                                                    }
-                                                  ]
-                                                  })
-                                .then(addedProduct => {
-                                console.log('product added success: ', addedProduct)
-                                })
-
-                               .catch(err => console.error('shopify addding product err:', err));
-
-  })
+    .catch(err => console.error('shopify addding product err:', err));
+});
 
 module.exports = router;
